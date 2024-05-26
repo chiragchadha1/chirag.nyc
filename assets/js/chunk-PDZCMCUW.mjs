a7bd83ea60422519bc042f04afc3e24b388d2762
Chirag Chadha <chiragkchadha@gmail.com>
2024-05-26 18:02:55 -0400

    update title fr

--- assets/js/chunk-PDZCMCUW.mjs
+++ assets/js/chunk-PDZCMCUW.mjs
@@ -1,2 +1,28 @@
-function e(a,t){return{bodyClassName:"framer-body-aIxubhzMw",breakpoints:[{hash:"41kc85",mediaQuery:"(min-width: 1920px)"},{hash:"8pznem",mediaQuery:"(min-width: 1440px) and (max-width: 1919px)"},{hash:"yj79nc",mediaQuery:"(min-width: 768px) and (max-width: 1439px)"},{hash:"1i9ztku",mediaQuery:"(max-width: 767px)"}],description:"Meet Chirag Chadha, a prominent Software Engineer specializing in app development, user-centric design, and AI. Dive into a portfolio where creativity meets code, showcasing projects that transform user experiences and push technological boundaries.",elements:{F4XeT2ldw:"about",IfBBZ5kQy:"nav",puP_dlZnC:"about-image"},framerSearch:{index:!0},socialImage:"https://framerusercontent.com/assets/Dx38d6xZjVvrvOtVxRNC1uEjrSo.jpg",title:"Chirag Chadha: Leading Software Engineer",viewport:"width=device-width"}}var o=1,d={exports:{metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},default:{type:"function",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{e as a,o as b,d as c};
+function e(a, t) {
+    return {
+        bodyClassName: 'framer-body-aIxubhzMw',
+        breakpoints: [
+            { hash: '41kc85', mediaQuery: '(min-width: 1920px)' },
+            { hash: '8pznem', mediaQuery: '(min-width: 1440px) and (max-width: 1919px)' },
+            { hash: 'yj79nc', mediaQuery: '(min-width: 768px) and (max-width: 1439px)' },
+            { hash: '1i9ztku', mediaQuery: '(max-width: 767px)' },
+        ],
+        description:
+            'Meet Chirag Chadha, a prominent Software Engineer specializing in app development, user-centric design, and AI. Dive into a portfolio where creativity meets code, showcasing projects that transform user experiences and push technological boundaries.',
+        elements: { F4XeT2ldw: 'about', IfBBZ5kQy: 'nav', puP_dlZnC: 'about-image' },
+        framerSearch: { index: !0 },
+        socialImage: 'https://framerusercontent.com/assets/Dx38d6xZjVvrvOtVxRNC1uEjrSo.jpg',
+        title: 'Chirag Chadha - Software Engineer',
+        viewport: 'width=device-width',
+    };
+}
+var o = 1,
+    d = {
+        exports: {
+            metadataVersion: { type: 'variable', annotations: { framerContractVersion: '1' } },
+            default: { type: 'function', annotations: { framerContractVersion: '1' } },
+            __FramerMetadata__: { type: 'variable' },
+        },
+    };
+export { e as a, o as b, d as c };
 //# sourceMappingURL=chunk-PDZCMCUW.mjs.map
