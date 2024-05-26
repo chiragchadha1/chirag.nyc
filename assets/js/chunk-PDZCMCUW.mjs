73aa196c75c862a9702b5180c6d7d2044cbf3ab4
Chirag Chadha <chiragkchadha@gmail.com>
2024-05-26 18:13:26 -0400

    fix SRC

--- assets/js/chunk-PDZCMCUW.mjs
+++ assets/js/chunk-PDZCMCUW.mjs
@@ -11,7 +11,7 @@ function e(a, t) {
             'Meet Chirag Chadha, a prominent Software Engineer specializing in app development, user-centric design, and AI. Dive into a portfolio where creativity meets code, showcasing projects that transform user experiences and push technological boundaries.',
         elements: { F4XeT2ldw: 'about', IfBBZ5kQy: 'nav', puP_dlZnC: 'about-image' },
         framerSearch: { index: !0 },
-        socialImage: 'https://framerusercontent.com/assets/Dx38d6xZjVvrvOtVxRNC1uEjrSo.jpg',
+        socialImage: 'https://chirag.nyc/assets/img/chirag-chadha-speaking-at-generate-showcase.jpg',
         title: 'Chirag Chadha - Software Engineer',
         viewport: 'width=device-width',
     };
